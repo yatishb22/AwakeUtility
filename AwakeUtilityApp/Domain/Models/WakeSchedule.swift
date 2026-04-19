a95777770b5bd4b2af77dab2fad52da5688c0f03
kian woon <wiserly@hotmail.com>
2026-04-19 23:38:27 +0800

    feat: add data migration for legacy schedule format
    Auto-migrate hour/minute/holdMinutes to startHour/startMinute/endHour/endMinute.
    Custom Codable with encode(to:) for forward-compatible serialization.

--- AwakeUtilityApp/Domain/Models/WakeSchedule.swift
+++ AwakeUtilityApp/Domain/Models/WakeSchedule.swift
@@ -1,6 +1,6 @@
 import Foundation
 
-struct WakeSchedule: Identifiable, Codable, Hashable {
+struct WakeSchedule: Identifiable, Hashable {
     let id: UUID
     var label: String
     var isEnabled: Bool
@@ -37,5 +37,53 @@ struct WakeSchedule: Identifiable, Codable, Hashable {
         let start = String(format: "%02d:%02d", startHour, startMinute)
         let end = String(format: "%02d:%02d", endHour, endMinute)
         return "\(start) - \(end)"
+    }
+}
+
+extension WakeSchedule: Codable {
+    private enum CodingKeys: String, CodingKey {
+        case id, label, isEnabled, startHour, startMinute, endHour, endMinute, repeatDays, requiresACPower
+        case hour, minute, holdMinutes
+    }
+
+    init(from decoder: Decoder) throws {
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+
+        id = try container.decode(UUID.self, forKey: .id)
+        label = try container.decode(String.self, forKey: .label)
+        isEnabled = try container.decode(Bool.self, forKey: .isEnabled)
+        repeatDays = try container.decode(Set<Weekday>.self, forKey: .repeatDays)
+        requiresACPower = try container.decode(Bool.self, forKey: .requiresACPower)
+
+        if let sh = try? container.decode(Int.self, forKey: .startHour) {
+            startHour = sh
+            startMinute = try container.decode(Int.self, forKey: .startMinute)
+            endHour = try container.decode(Int.self, forKey: .endHour)
+            endMinute = try container.decode(Int.self, forKey: .endMinute)
+        } else {
+            let legacyHour = try container.decode(Int.self, forKey: .hour)
+            let legacyMinute = try container.decode(Int.self, forKey: .minute)
+            let legacyHold = try container.decode(Int.self, forKey: .holdMinutes)
+
+            startHour = legacyHour
+            startMinute = legacyMinute
+            var endTotal = legacyHour * 60 + legacyMinute + legacyHold
+            if endTotal >= 24 * 60 { endTotal = endTotal % (24 * 60) }
+            endHour = endTotal / 60
+            endMinute = endTotal % 60
+        }
+    }
+
+    func encode(to encoder: Encoder) throws {
+        var container = encoder.container(keyedBy: CodingKeys.self)
+        try container.encode(id, forKey: .id)
+        try container.encode(label, forKey: .label)
+        try container.encode(isEnabled, forKey: .isEnabled)
+        try container.encode(startHour, forKey: .startHour)
+        try container.encode(startMinute, forKey: .startMinute)
+        try container.encode(endHour, forKey: .endHour)
+        try container.encode(endMinute, forKey: .endMinute)
+        try container.encode(repeatDays, forKey: .repeatDays)
+        try container.encode(requiresACPower, forKey: .requiresACPower)
     }
 }
